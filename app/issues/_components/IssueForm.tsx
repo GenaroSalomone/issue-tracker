3b002124d42a8e4c4f35d746d289ff054e34d7ef
Genaro Salomone <genarosalomone@gmail.com>
2023-12-13 18:53:02 -0300

    Implement update issue in client

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -30,9 +30,12 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 
   const onSubmit = handleSubmit( async (data) => {
     try {
-      setIsSubmitting(true);
-      await axios.post('/api/issues', data);
-      router.push('/issues');
+      if ( issue )
+        await axios.patch('/api/issues/' + issue.id, data)
+      else
+        setIsSubmitting(true);
+        await axios.post('/api/issues', data);
+        router.push('/issues');
     } catch (error) {
       setIsSubmitting(false);
       setError('An unexpected error ocurred.');
@@ -65,7 +68,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
           { errors.description?.message }
         </ErrorMessage>
         <Button disabled={isSubmitting}>
-          Submit New Issue {isSubmitting && <Spinner/>}
+          { issue ? 'Update Issue' : 'Submit New Issue'}
+          {isSubmitting && <Spinner/>}
         </Button>
       </form>
     </div>
