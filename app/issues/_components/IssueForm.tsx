300493808efe20f9f40da9c88c84e92e8d110d72
Genaro Salomone <genarosalomone@gmail.com>
2023-12-13 18:22:14 -0300

    Build route to update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -7,7 +7,7 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
@@ -18,12 +18,12 @@ const SimpleMDE = dynamic(
   { ssr: false } //not to render component in server side
 );
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const router = useRouter();
   const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
-    resolver: zodResolver( createIssueSchema )
+    resolver: zodResolver( issueSchema )
   });
   const [error, setError] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
