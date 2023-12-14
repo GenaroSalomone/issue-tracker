ba6828b73acc4fc5c9dd5a81c4bcd929b0fe217b
Genaro Salomone <genarosalomone@gmail.com>
2023-12-14 18:06:56 -0300

    Secure app

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,11 +4,14 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
 interface Props {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({
     where: {id: parseInt( params.id )}
   });
@@ -16,19 +19,19 @@ const IssueDetailPage = async ({ params }: Props) => {
   if ( !issue )
     notFound();
 
-  //md from radixUI equivalent to lg (laptop)
   return (
     <Grid columns={{ initial: "1", sm:"5" }} gap="5">
       {/* md from radixUI equivalent to lg (laptop) from tailwind */}
       <Box className="md:col-span-4">
         <IssueDetails issue={ issue }></IssueDetails>
       </Box>
-      <Box>
+      { session && (<Box>
         <Flex direction="column" gap="4">
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
       </Box>
+      )}
     </Grid>
   )
 }
