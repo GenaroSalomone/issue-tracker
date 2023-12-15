ce96b6e83e32fc0fddf5ee5b3888ef8619a6243f
Genaro Salomone <genarosalomone@gmail.com>
2023-12-15 09:09:31 -0300

    Set up React Query
    Problem to solve: every time asignee select component, is needed to refetch list of users from backend, so here comes React Query.

---
+++ app/QueryClientProvider.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React, { PropsWithChildren } from 'react'
+import { QueryClient, QueryClientProvider as ReactQueryClientProvider } from '@tanstack/react-query';
+
+//Cache for storing data obtained from backend
+const queryClient = new QueryClient();
+
+const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ReactQueryClientProvider client={queryClient}>
+
+    </ReactQueryClientProvider>
+  )
+}
+
+export default QueryClientProvider
